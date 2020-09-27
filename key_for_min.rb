9792c5c784060d6dba8e39940edbb74f3cdcdb5c
Justin Clifford <justoon7.jc@gmail.com>
2020-09-27 20:43:40 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ require 'pry'
 def key_for_min_value(name_hash)
   min_key = 0
   name_hash.each do |key, value|
-    if name_hash[value] > min_key
+    if name_hash[key] > min_key
       min_key = name_hash[key]
       binding.pry
     end
