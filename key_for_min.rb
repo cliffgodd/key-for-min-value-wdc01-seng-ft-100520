dd0fa88f60abcb53b01e19ea91fc195e70b2e6e3
Justin Clifford <justoon7.jc@gmail.com>
2020-09-27 21:27:18 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,10 +4,10 @@ require 'pry'
 
 def key_for_min_value(name_hash)
   min_key = nil
-  min_value = 501
+  min_value = nil
   name_hash.each do |key, value|
     #binding.pry
-    if value < min_value
+    if value < min_value or min_value == nil
       min_value = value
       min_key = key
     end
